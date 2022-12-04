c412b6730a3e6e319e4f477d03d7ae971d1f2b97
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-12-04 10:19:48 -0300

    Adding the Express.js settings.

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import serverless from 'serverless-http';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', routes);
+
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.status(404).send();
+});
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.status(err.status || 500).send();
+});
+
+export const handler = serverless(app);
